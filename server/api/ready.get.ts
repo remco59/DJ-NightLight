563605be333a41a5da7e65b904a07be4caa61c55
Remco Land <remcoland59@gmail.com>
2026-09-19 17:18:06 +0200

    ci: add quality gates and deploy workflows

--- server/api/ready.get.ts
+++ server/api/ready.get.ts
@@ -1,6 +1,6 @@
 import { sql } from '../utils/db'
 
-export default defineEventHandler(async () => {
+export default defineEventHandler(async (event) => {
   try {
     await sql`select 1`
     return {
