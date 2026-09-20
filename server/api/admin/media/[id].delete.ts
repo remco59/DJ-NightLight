37e3b5b4a0fd33a5d0ec844dc5f9650dc8e5c7c4
Remco Land <remcoland59@gmail.com>
2026-09-20 17:36:29 +0200

    Phase 6: protect media deletion with usage checks

---
+++ server/api/admin/media/[id].delete.ts
@@ -0,0 +1,28 @@
+import { eq } from 'drizzle-orm'
+import { mediaAssets } from '../../../../db/schema'
+import { db } from '../../../utils/db'
+import { getMediaUsage } from '../../../utils/media-library'
+import { getMediaStorage } from '../../../utils/media-storage'
+import { requireStaff } from '../../../utils/require-staff'
+
+export default defineEventHandler(async (event) => {
+  await requireStaff(event, ['owner', 'manager', 'content_editor'])
+  const id = getRouterParam(event, 'id')
+  if (!id) throw createError({ statusCode: 400, statusMessage: 'Media id is required' })
+
+  const usage = await getMediaUsage(id)
+  if (!usage) throw createError({ statusCode: 404, statusMessage: 'Media asset not found' })
+  if (usage.references.length) {
+    throw createError({
+      statusCode: 409,
+      statusMessage: 'Media asset is still in use',
+      data: { references: usage.references },
+    })
+  }
+
+  await db.delete(mediaAssets).where(eq(mediaAssets.id, id))
+  const storage = getMediaStorage()
+  await storage.delete(usage.asset.storageKey)
+  await storage.delete(usage.asset.thumbnailKey)
+  return { ok: true }
+})
