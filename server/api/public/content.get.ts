0b4f54304de3ec22b873b800ecc312bf71df990f
Remco Land <remcoland59@gmail.com>
2026-09-22 17:19:01 +0200

    Resolve public-site copy defaults

--- server/api/public/content.get.ts
+++ server/api/public/content.get.ts
@@ -1,14 +1,14 @@
 import { eq } from 'drizzle-orm'
 import { siteContent } from '../../../db/schema'
 import { db } from '../../utils/db'
-import { defaultSiteContent } from '../../utils/site-content-defaults'
+import { defaultSiteContent, resolvePublicCopy } from '../../utils/site-content-defaults'
 
 export default defineEventHandler(async () => {
   const [content] = await db.select().from(siteContent).where(eq(siteContent.key, 'default')).limit(1)
 
   return {
     content: content
-      ? { ...content, key: undefined, updatedAt: undefined }
+      ? { ...content, publicCopy: resolvePublicCopy(content.publicCopy), key: undefined, updatedAt: undefined }
       : defaultSiteContent,
   }
 })
